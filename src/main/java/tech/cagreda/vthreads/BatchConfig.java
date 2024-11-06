61e9d1738492ec16de4221ec08aeeaf2527859af
agredac <crIstiana@ecsfin.com>
2024-11-06 16:56:09 -0500

    Read TxInfAndSts

--- src/main/java/tech/cagreda/vthreads/BatchConfig.java
+++ src/main/java/tech/cagreda/vthreads/BatchConfig.java
@@ -1,13 +1,14 @@
 package tech.cagreda.vthreads;
 
-import com.latam.ecs.canonical.model.paymentresponses.Document;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
-import com.latam.ecs.canonical.model.paymentresponses.OriginalPaymentInstruction40;
-import com.latam.ecs.canonical.model.paymentresponses.PaymentTransaction129;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
@@ -18,6 +19,7 @@ import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.batch.item.xml.StaxEventItemReader;
+import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -55,12 +57,15 @@ public class BatchConfig {
 
   @Bean
   public Step processFileStep(
-      ItemReader<PaymentTransaction129> reader,
-      ItemProcessor<PaymentTransaction129, String> processor,
+      ItemReader<TxInfAndSts> reader,
+      // ItemReader<Document> reader,
+      ItemProcessor<TxInfAndSts, String> processor,
+      // ItemProcessor<Document, String> processor,
       ItemWriter<String> writer) {
 
     return new StepBuilder("processFileStep", jobRepository)
-        .<PaymentTransaction129, String>chunk(1000, transactionManager)
+        .<TxInfAndSts, String>chunk(1, transactionManager)
+        // .<Document, String>chunk(1000, transactionManager)
         .reader(reader)
         .processor(processor)
         .writer(writer)
@@ -70,30 +75,31 @@ public class BatchConfig {
 
   @Bean
   @StepScope
-  public StaxEventItemReader<PaymentTransaction129> reader() {
-    StaxEventItemReader<PaymentTransaction129> reader = new StaxEventItemReader<>();
-    reader.setResource(new ClassPathResource("CANONICALPain002V12.xml"));
-    reader.setFragmentRootElementName("TxInfAndSts"); // Root element for each record
+  public StaxEventItemReader<TxInfAndSts> reader() {
 
     Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-    marshaller.setClassesToBeBound(PaymentTransaction129.class); // Bind XML to Employee class
-    reader.setUnmarshaller(marshaller);
+    marshaller.setClassesToBeBound(TxInfAndSts.class);
 
-    return reader;
+    return new StaxEventItemReaderBuilder<TxInfAndSts>()
+        .name("itemReader")
+        .resource(new ClassPathResource("CANONICALPain002V12.xml"))
+        .addFragmentRootElements("TxInfAndSts")
+        .unmarshaller(marshaller)
+        .build();
   }
 
   // ItemProcessor Bean (custom implementation)
   @Bean
   @StepScope
-  public ItemProcessor<PaymentTransaction129, String> processor() {
+  public ItemProcessor<TxInfAndSts, String> processor() {
+    // public ItemProcessor<Document, String> processor() {
     return line -> {
-
-      Optional<PaymentRequestModel> result = referenceTablePortOut.findMessageId(
-              line.getOrgnlEndToEndId());
+      Optional<PaymentRequestModel> result =
+          referenceTablePortOut.findMessageId(line.getOrgnlEndToEndId());
 
       return result
-              .map(paymentRequestModel -> line + "," + paymentRequestModel.bankFileName())
-              .orElseGet(() -> line + ",,");
+          .map(paymentRequestModel -> line + "," + paymentRequestModel.bankFileName())
+          .orElseGet(() -> line + ",,");
     };
   }
 
---
+++ src/main/java/tech/cagreda/vthreads/TxInfAndSts.java
@@ -0,0 +1,30 @@
+package tech.cagreda.vthreads;
+
+import com.latam.ecs.canonical.model.paymentresponses.*;
+import jakarta.xml.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+ @XmlRootElement(name = "TxInfAndSts")
+/*@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "TxInfAndSts")*/
+public class TxInfAndSts {
+
+
+  @XmlElement(name = "OrgnlEndToEndId")
+  protected String orgnlEndToEndId;
+
+  public TxInfAndSts() {}
+
+
+  public String getOrgnlEndToEndId() {
+    return this.orgnlEndToEndId;
+  }
+
+  public void setOrgnlEndToEndId(String value) {
+    this.orgnlEndToEndId = value;
+  }
+
+}
